90f9653c411134847d09328a9ba7e63d53b5d275
faizsiddiq <faizsiddiq786@gmail.com>
2020-10-27 19:08:52 -0700

    abdullah task

--- src/test/java/org/cts/GitProject.java
+++ src/test/java/org/cts/GitProject.java
@@ -6,6 +6,10 @@ public class GitProject {
 		
 		System.out.println("hello");
 		System.out.println("sheik");
+		System.out.println("day1");
+		System.out.println("day2");
+		System.out.println("day3");
+		System.out.println("day4");
 		
 	}
 
